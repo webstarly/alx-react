fdc1f6ff7fdf7dcc539d88d85d8483453a5f0651
webstarly <edidionge.ukem@gmail.com>
2024-01-16 14:58:57 +0000

    task_0

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './holberton-logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="Holberton Logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <footer className="App-footer">
+        <p>Copyright 2020 - Holberton School</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
